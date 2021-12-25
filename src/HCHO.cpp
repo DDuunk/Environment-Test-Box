352e866d42f8aa7d64d386feb522a225c3eed91c
DDuunk <dylanduunk@live.nl>
2021-12-25 21:48:46 +0100

    feat(hcho): improve logging

--- src/HCHO.cpp
+++ src/HCHO.cpp
@@ -11,20 +11,19 @@ bool HCHO::begin() {
 void HCHO::calibrate() {
   uint16_t sensorValue = analogRead(this->sensorPin);
   this->calibrationValue = (1023.0 / sensorValue) - 1;
-  Serial.print("HCHO - R0 = ");
+
+  Serial.print("|HCHO| [Calibration] R0: ");
   Serial.println(calibrationValue);
 }
 
 double HCHO::readHCHO() {
   uint16_t sensorValue = analogRead(this->sensorPin);
   double Rs = (1023.0 / sensorValue) - 1;
-
-  Serial.print("HCHO - Rs = ");
-  Serial.println(Rs);
-
   double ppm = pow(10.0, ((log10(Rs / this->calibrationValue) - 0.0827) / (-0.4807)));
 
-  Serial.print("HCHO - HCHO ppm = ");
+  Serial.print("|HCHO| Rs: ");
+  Serial.print(Rs);
+  Serial.print(", ppm = ");
   Serial.println(ppm);
 
   return ppm;
